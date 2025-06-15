bbb8ad62e39e8ff2aa2bd2ae9f65ab7e717b3c43
Mak35988 <Mayurrai8085213293>
2025-06-15 10:46:12 +0530

    removed the pass.py

--- pass.py
+++
@@ -1,7 +0,0 @@
-# Run this in Python shell to create initial admin
-import bcrypt
-password = "admin123".encode('utf-8')
-hashed = bcrypt.hashpw(password, bcrypt.gensalt()).decode('utf-8')
-
-# Then insert into admins table using your MySQL client:
-# INSERT INTO admins (username, password) VALUES ('admin', '<hashed-password>');
