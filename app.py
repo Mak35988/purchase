b4aec9b899aebde41a642fed91c1f06a6422fab2
Mak35988 <Mayurrai8085213293>
2025-06-15 11:04:15 +0530

    update the port in app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session, j
 import mysql.connector
 import bcrypt
 from functools import wraps
+import os
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
